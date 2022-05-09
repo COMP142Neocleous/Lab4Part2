bf74cd41aefda8c047b40731b18b0f9e0d7c8b72
Andreas Neocleous <a.neocleous12@hotmail.com>
2022-05-09 08:09:28 +0300

    Fixed bug #3

--- Lab4Part2/Matrix.cpp
+++ Lab4Part2/Matrix.cpp
@@ -51,7 +51,7 @@ double sumOfCol(const double mat[][MAX_COL], const int column, const int maxRow)
 
 	for (int i = 0; i < maxRow; i++)
 	{
-		sum = mat[i][column];
+		sum += mat[i][column];
 	}
 
 	return sum;
